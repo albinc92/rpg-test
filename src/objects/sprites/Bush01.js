c4a6af82258d69bdf35a812d015e2b3281b9209f
Albin Carlson <albin_carlson92@hotmail.com>
2025-10-02 12:54:53 +0200

    removed trash functions from static objects

--- src/objects/sprites/Bush01.js
+++ src/objects/sprites/Bush01.js
@@ -6,12 +6,6 @@ class Bush01 extends Bush {
     constructor(options = {}) {
         super({
             spriteSrc: 'assets/objects/bushes/bush-01.png',
-            name: 'Green Bush',
-            scale: options.scale || 0.45,
-            bushType: 'generic',
-            density: 'medium',
-            x: options.x,
-            y: options.y,
             ...options
         });
     }
--- src/objects/sprites/Tree01.js
+++ src/objects/sprites/Tree01.js
@@ -6,13 +6,7 @@ class Tree01 extends Tree {
     constructor(options = {}) {
         super({
             spriteSrc: 'assets/objects/trees/tree-01.png',
-            name: 'Oak Tree',
-            scale: options.scale || 1.0,
-            treeType: 'deciduous',
-            height: 'tall',
-            foliageDensity: 0.8,
-            x: options.x,
-            y: options.y,
+            collisionExpandTop: -45,
             ...options
         });
     }
